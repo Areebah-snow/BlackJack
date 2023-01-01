3ecbf4e7536adb969ca21e44322060fc8be655ee
Areebah-snow <Areebah-snow>
2023-01-01 15:30:25 +0100

    Regenerating startGame

--- blackjack.js
+++ blackjack.js
@@ -1,30 +1,37 @@
-let firstCard =  getRandomCard()
-let secondCard =  getRandomCard()
-let sum = firstCard+secondCard
+let sum = 0
+let cards = []
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-let cards = [firstCard, secondCard] //array 
-
+ 
+console.log(cards) 
 
 //Allowing the code to generate random numbers
 function getRandomCard(){
     
      let randomNumber = Math.floor(Math.random() * 13 ) + 1
-     if(randomNumber === 1 )
-         return 11
+     if(randomNumber === 1 ){
+        return 11
+     }  
      else if (randomNumber > 10  )
+     {
         return 10
-    
-    else
+     }
+    else{
         return randomNumber
-    
+    }
 }
 
 function startGame(){
+    isAlive = true
+    let firstCard =  getRandomCard()
+    let secondCard =  getRandomCard()
+    let cards = [firstCard, secondCard]
+    let sum  = firstCard + secondCard
+    isAlive =  true
     renderGame()
 }
 
