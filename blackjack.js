ce940a2f609d0a4b5d037db04d24006f6f576d84
Areebah-snow <Areebah-snow>
2023-01-01 15:12:30 +0100

    Condition on getRandomCard

--- blackjack.js
+++ blackjack.js
@@ -12,8 +12,16 @@ let cards = [firstCard, secondCard] //array
 
 //Allowing the code to generate random numbers
 function getRandomCard(){
- let randomNumber = Math.floor(Math.random() * 13 ) + 1
-    return randomNumber
+    
+     let randomNumber = Math.floor(Math.random() * 13 ) + 1
+     if(randomNumber === 1 )
+         return 11
+     else if (randomNumber > 10  )
+        return 10
+    
+    else
+        return randomNumber
+    
 }
 
 function startGame(){
