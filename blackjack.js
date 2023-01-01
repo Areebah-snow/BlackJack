021fdfd7d03753cbc8b99763f2c11fa2fa038a46
Areebah-snow <Areebah-snow>
2023-01-01 18:40:37 +0100

    Finishing touch on newCard

--- blackjack.js
+++ blackjack.js
@@ -1,44 +1,47 @@
-let sum = 0
 let cards = []
+let sum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
- 
-console.log(cards) 
+let playerName =  Shima
+let playerChips = 241
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = playerName + ": $" + playerChips
+
 
 //Allowing the code to generate random numbers
 function getRandomCard(){
     
      let randomNumber = Math.floor(Math.random() * 13 ) + 1
-     if(randomNumber === 1 ){
-        return 11
-     }  
+     if(randomNumber === 1 )
+         return 11
      else if (randomNumber > 10  )
-     {
         return 10
-     }
-    else{
+    
+    else
         return randomNumber
-    }
+    
 }
 
 function startGame(){
     isAlive = true
     let firstCard =  getRandomCard()
     let secondCard =  getRandomCard()
-    let cards = [firstCard, secondCard]
-    let sum  = firstCard + secondCard
-    isAlive =  true
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    console.log(firstCard, secondCard)
     renderGame()
 }
 
 
 function renderGame(){
-    cardsEl.textContent = "Cards: "
-
+    cardsEl.textContent = "Cards: " 
+    console.log(cards)
+  
     for(let i = 0; i < cards.length ; i++){
         cardsEl.textContent +=(cards[i]) + " "
     }
@@ -60,13 +63,15 @@ function renderGame(){
     messageEl.textContent = message
 }
 
-function newCard(){
+function newCard(){ 
+
     console.log("Drawing a new card from the deck")
+    if(isAlive === true && hasBlackJack === false){
     let card = getRandomCard()
     sum+=card
-
     cards.push(card)
     console.log(cards)
     renderGame()  
+    }
 }
 
